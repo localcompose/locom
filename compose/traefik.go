2f373d34d8bf8ec9403c2fbffa3490d0fa18eca9
moudrick <moudrick@gmail.com>
2025-08-25 00:09:31 +0000

    testable: cd proxy; docker compose up

--- compose/traefik.go
+++ compose/traefik.go
@@ -1,5 +1,9 @@
 package compose
 
+import (
+	"gopkg.in/yaml.v3"
+)
+
 func GetTraefikCompose(networkName string) ComposeFile {
 	composeFIle := ComposeFile{
 		Networks: map[string]ExternalNetwork{
@@ -31,38 +35,63 @@ func GetTraefikCompose(networkName string) ComposeFile {
 					"./certs:/certs:ro",
 				},
 				Networks: []string{networkName},
-				Labels:   nil,
 			},
 		},
 	}
 
 	isHttps := true
 	s := composeFIle.Services["traefik"]
-	if isHttps {
-		s.Labels = map[string]string{
-			// https
-			// # Unsecured HTTP router (redirect to HTTPS)
-			// "traefik.http.routers.traefik.rule": `&traefik-rule >-
-			// 	Host(` + "`proxy.locom.self`)",
-			"traefik.http.routers.traefik.rule":        "Host(`proxy.locom.self`)",
-			"traefik.http.routers.traefik.entrypoints": "web",
-			"traefik.http.routers.traefik.middlewares": "redirect-to-https",
 
-			// # HTTPS router (certresolver + secure dashboard)
-			"traefik.http.middlewares.redirect-to-https.redirectscheme.scheme": "https",
-			// "traefik.http.routers.traefik-secure.rule":                         "*traefik-rule",
-			"traefik.http.routers.traefik-secure.rule":        "Host(`proxy.locom.self`)",
-			"traefik.http.routers.traefik-secure.entrypoints": "websecure",
-			"traefik.http.routers.traefik-secure.service":     "api@internal",
-			"traefik.http.routers.traefik-secure.tls":         "true",
+	if isHttps {
+		httpRule := "Host(`proxy.locom.self`)"
+		s.LabelsNode = &yaml.Node{
+			Kind: yaml.MappingNode,
+			Content: []*yaml.Node{
+				// proxy
+				{Kind: yaml.ScalarNode, Value: "traefik.enable"},
+				{Kind: yaml.ScalarNode, Value: "true"},
+
+				// http
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.rule"},
+				{Kind: yaml.ScalarNode, Value: httpRule},
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.entrypoints"},
+				{Kind: yaml.ScalarNode, Value: "web"},
+
+				// redirect
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.middlewares"},
+				{Kind: yaml.ScalarNode, Value: "redirect-to-https"},
+
+				// https
+				{Kind: yaml.ScalarNode, Value: "traefik.http.middlewares.redirect-to-https.redirectscheme.scheme"},
+				{Kind: yaml.ScalarNode, Value: "https"},
+
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik-secure.rule"},
+				{Kind: yaml.ScalarNode, Value: httpRule},
+
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik-secure.entrypoints"},
+				{Kind: yaml.ScalarNode, Value: "websecure"},
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik-secure.service"},
+				{Kind: yaml.ScalarNode, Value: "api@internal"},
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik-secure.tls"},
+				{Kind: yaml.ScalarNode, Value: "true"},
+			},
 		}
 	} else {
-		s.Labels = map[string]string{
-			// http
-			"traefik.enable":                           "true",
-			"traefik.http.routers.traefik.rule":        "Host(`proxy.locom.self`)",
-			"traefik.http.routers.traefik.service":     "api@internal",
-			"traefik.http.routers.traefik.entrypoints": "web",
+		s.LabelsNode = &yaml.Node{
+			Kind: yaml.MappingNode,
+			Content: []*yaml.Node{
+				// proxy
+				{Kind: yaml.ScalarNode, Value: "traefik.enable"},
+				{Kind: yaml.ScalarNode, Value: "true"},
+
+				// http
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.rule"},
+				{Kind: yaml.ScalarNode, Value: "Host(`proxy.locom.self`)"},
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.service"},
+				{Kind: yaml.ScalarNode, Value: "api@internal"},
+				{Kind: yaml.ScalarNode, Value: "traefik.http.routers.traefik.entrypoints"},
+				{Kind: yaml.ScalarNode, Value: "web"},
+			},
 		}
 	}
 	composeFIle.Services["traefik"] = s
--- compose/types.go
+++ compose/types.go
@@ -1,22 +1,30 @@
 package compose
 
+import "gopkg.in/yaml.v3"
+
+// ComposeFile represents a Docker Compose file
 type ComposeFile struct {
 	Version  string                     `yaml:"version,omitempty"`
 	Networks map[string]ExternalNetwork `yaml:"networks"`
 	Services map[string]Service         `yaml:"services"`
 }
 
+// Service represents a single service in Compose
 type Service struct {
-	Image         string            `yaml:"image,omitempty"`
-	ContainerName string            `yaml:"container_name,omitempty"`
-	Restart       string            `yaml:"restart,omitempty"`
-	Command       []string          `yaml:"command,omitempty"`
-	Ports         []string          `yaml:"ports,omitempty"`
-	Volumes       []string          `yaml:"volumes,omitempty"`
-	Networks      []string          `yaml:"networks,omitempty"`
-	Labels        map[string]string `yaml:"labels,omitempty"`
+	Image         string   `yaml:"image,omitempty"`
+	ContainerName string   `yaml:"container_name,omitempty"`
+	Restart       string   `yaml:"restart,omitempty"`
+	Command       []string `yaml:"command,omitempty"`
+	Ports         []string `yaml:"ports,omitempty"`
+	Volumes       []string `yaml:"volumes,omitempty"`
+	Networks      []string `yaml:"networks,omitempty"`
+
+	// Use either Labels (unordered map) or LabelsNode (ordered with optional anchors)
+	// Labels     map[string]string `yaml:"labels,omitempty"`
+	LabelsNode *yaml.Node `yaml:"labels,omitempty"`
 }
 
+// ExternalNetwork represents an external network
 type ExternalNetwork struct {
 	External bool `yaml:"external"`
 }
